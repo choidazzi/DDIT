60a1306a02146bae06b5029414a0f976256a910a
ChoiDazzi <choidazzi@gmail.com>
2023-05-31 08:59:25 +0900

    Revert "~0531"
    This reverts commit 5844155ebfc87bd1e257f49f772f6dae39220bf6.

--- web programming/교안/5_ajax.sql
+++
@@ -1,8 +0,0 @@
-select * from lprod; 
--- parameter X / result O (java - list / type - vo)
-
-select prod_id, prod_name from prod where prod_lgu = 'lprod_gu';
--- parameter = String / result = (java - list / type - vo)
-
-select * from prod where prod_id = 'P101000001';
--- parameter = String / result = vo
